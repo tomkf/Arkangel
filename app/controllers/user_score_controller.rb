f8438b325d7d43e5444042db15ac3ad2a3747525
Thomas Ferris <thomaskeithferris@gmail.com>
2019-05-29 12:04:02 -0400

    refactor of graph method

--- app/controllers/user_score_controller.rb
+++ app/controllers/user_score_controller.rb
@@ -1,21 +1,7 @@
 class UserScoreController < ApplicationController
 
-
-
   def index
-
-    @userscoresweek = UserScore.last(28)
-
   end
-
-
-  def new
-  end
-
-def index
-
-end
-
 
 
   def new
@@ -27,11 +13,26 @@ end
     score.save
   end
 
-  def graph_display
+  def graph  #make associated view
+   @user_score_params = UserScore.last
 
+   if params(:date) == "Daily"
+    elsif params(:date) == "Weekly"
+     @user_score_params = UserScore.last(7)
+    elsif params(:date) == "Monthly"
+     @user_score_params = UserScore.last(25)  #make this logic work w/ helper function for MONTHS
+    end
 
-    weekly_scores = UserScore.last(7)
-    graph_array =
+   @health_score = []
+   @bmi = []
+   @dates = []
 
+   @user_score_params.each do |score|
+     health_score << score.health_score.round
+     bmi << score.bmi
+     dates << score.logdate
+   end
   end
+
+
 end
