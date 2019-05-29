1c7495ed6facecb8c2851501070ba586dc6131a3
Gustaf Folkmar <gustaf@folkmar.se>
2019-05-29 16:57:13 -0400

    Route fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  root to: 'user_score#index'
+  root to: 'pages#home'
 
   get "user_score/graph", to: 'user_score#graph', as: 'graph'
   get "user_score/day_graph", to: 'user_score#day_graph', as: 'day_graph'
