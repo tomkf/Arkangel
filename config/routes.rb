126387130f7f9a84c26281856d5254ee901c9f76
Thomas Ferris <thomaskeithferris@gmail.com>
2019-05-31 11:07:58 -0400

    very obnoxious debugs from my previous changes

--- config/routes.rb
+++ config/routes.rb
@@ -8,15 +8,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "user_score/graph", to: 'user_score#graph', as: 'graph'
   get "user_score/day_graph", to: 'user_score#day_graph', as: 'day_graph'
-<<<<<<< HEAD
-  get "user_score/update_score", to: 'user_score#update_score', as: 'update_score'
-  get "user_score/exercise_score", to: 'user_score#exercise_score', as: 'exercise_score'
-  get "user_score/food_score", to: 'user_score#food_score', as: 'food_score'
-
-resources :user do
-resources :user_param
-end
-=======
->>>>>>> 0f6d66d31bddb9ff9ce0002952a12733e52abd48
 
 end
