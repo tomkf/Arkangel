8b73b17f409ae75a47f98c06979fadf27af5ebc4
Thomas Ferris <thomaskeithferris@gmail.com>
2019-06-03 16:15:23 -0400

    update to mailer methods

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,8 +6,8 @@ class UserMailer < ApplicationMailer
   #   en.user_mailer.welcome.subject
   #
   def welcome
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+    @user = params[:user] # Instance variable => available in view
+    mail(to: @user.email, subject: 'Welcome to Le Wagon')
+    # This will render a view in `app/views/user_mailer`!
   end
 end
--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -3,7 +3,9 @@ class UserMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/user_mailer/welcome
   def welcome
-    UserMailer.welcome
+    user = User.first
+    # This is how you pass value to params[:user] inside mailer definition!
+    UserMailer.with(user: user).welcome
   end
 
 end
