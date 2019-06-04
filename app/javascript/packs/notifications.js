b1e9ce94043c444c57377bdc99de6ce4f134bfde
Gustaf Folkmar <gustaf@folkmar.se>
2019-06-04 15:58:49 -0400

    Notifications done

---
+++ app/javascript/packs/notifications.js
@@ -0,0 +1,40 @@
+import Toastify from 'toastify-js'
+
+
+
+const authToken = document.querySelector('meta[name="csrf-token"]').content;
+
+const url = '/notification'
+
+const initNotify = () =>  { setInterval (() => {
+  fetch(url, {
+      method: "GET",
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Requested-With': 'XMLHttpRequest',
+        'X-CSRF-Token': authToken
+      },
+      credentials: 'same-origin'
+    })
+    .then((response) => response.json())
+    .then((data) => {
+       //console.log(data)
+       if (data.score !== null) {
+       Toastify({
+          text: `New score: ${data.score}`,
+          duration: 10000,
+          destination: "/dashboard",
+          newWindow: true,
+          close: true,
+          gravity: "top", // `top` or `bottom`
+          positionLeft: false, // `true` or `false`
+          backgroundColor: "linear-gradient(to right, #00b09b, #96c93d)",
+          stopOnFocus: true // Prevents dismissing of toast on hover
+        }).showToast();
+      }
+    })
+  },500)
+};
+
+export { initNotify };
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_04_133802) do
-
+ActiveRecord::Schema.define(version: 2019_06_04_193918) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +47,7 @@ ActiveRecord::Schema.define(version: 2019_06_04_133802) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "notified_user", default: false
     t.index ["user_id"], name: "index_fitbit_scores_on_user_id"
   end
 
@@ -91,11 +91,11 @@ ActiveRecord::Schema.define(version: 2019_06_04_133802) do
     t.string "last_name"
     t.integer "age"
     t.string "smoker"
+    t.boolean "admin", default: false, null: false
     t.string "fitbit_access_token"
     t.string "fitbit_refresh_token"
     t.integer "fitbit_expires_at"
     t.string "fitbit_user_id"
-    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
