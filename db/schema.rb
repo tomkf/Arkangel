ca014bebee0501fe7127f0d5b2eabb3047d7321b
Jose Zea <josedavid.zea@gmail.com>
2019-06-04 12:59:09 -0400

    added the new migration file to fitbit scores

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_04_133802) do
+ActiveRecord::Schema.define(version: 2019_06_04_165630) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,8 @@ ActiveRecord::Schema.define(version: 2019_06_04_133802) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "sleep_score"
+    t.float "floors"
     t.index ["user_id"], name: "index_fitbit_scores_on_user_id"
   end
 
