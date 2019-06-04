ab1e2f79945d22ef8c4eb5238091cb34fefc3966
Joe Di Lillo <jdilillo@gmail.com>
2019-06-04 09:25:26 -0400

    Minor changes to sign-up form

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_03_173827) do
+ActiveRecord::Schema.define(version: 2019_06_03_180251) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  # Health_score table
-  # user_id
-  # health_score
-  # fitbit
-  # end
 
   create_table "user_params", force: :cascade do |t|
     t.float "bmi"
@@ -30,12 +24,6 @@ ActiveRecord::Schema.define(version: 2019_06_03_173827) do
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_user_params_on_user_id"
   end
-
-# user_scores
-# object with all the columns from fitbit. (everytime we fetch the data it will store a new object.)
-# fibit_date of these values (based on the users)
-# everytime a new line appears you run the algorithm.
-# end
 
   create_table "user_scores", force: :cascade do |t|
     t.float "health_score"
@@ -67,11 +55,11 @@ ActiveRecord::Schema.define(version: 2019_06_03_173827) do
     t.string "last_name"
     t.integer "age"
     t.string "smoker"
-    t.boolean "admin", default: false, null: false
     t.string "fitbit_access_token"
     t.string "fitbit_refresh_token"
     t.integer "fitbit_expires_at"
     t.string "fitbit_user_id"
+    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
