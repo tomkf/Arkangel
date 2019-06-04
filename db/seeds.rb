be2c0277ddddc44703367b7b46bdd0191e575969
Thomas Ferris <thomaskeithferris@gmail.com>
2019-06-04 15:41:46 -0400

    update to avg scores

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,12 +13,12 @@ puts 'Adding user score...'
 
 # Add score
 
-exercise_array = ["jogging", "brisk walk", "swimming", "weights" ]
+exercise_array = ["jogging", "brisk walk", "swimming", "weights", "biking", "basketball" ]
 
 
 
 poor_hash = { user: user,
-  bmi: 30,
+  bmi: 31,
   overall_sleep: rand(280..350),
   awaken_sleep: 35,
   rem_sleep: 50,
@@ -31,7 +31,7 @@ poor_hash = { user: user,
   floors: rand(0..5),
   exercise_time: rand(5..25),
   exercise_km: rand(0..6),
-  exercise_cal: rand(1000..2600)
+  exercise_cal: rand(1000..2600),
   water: rand(1800..2800),
   carbs: rand(80..360),
   fat: rand(80..150),
@@ -45,31 +45,31 @@ poor_hash = { user: user,
   logdate: '2019-06-01'}
 
 average_hash = { user: user,
-  bmi: 30,
-  overall_sleep: 360,
+  bmi: 29,
+  overall_sleep: rand(280..350),
   awaken_sleep: 35,
   rem_sleep: 50,
   light_sleep: 15,
   deep_sleep: 28,
-  heart_rate: 70,
-  steps: 6000,
-  active_minutes: 25,
-  exercise_type: "yoga",
-  floors: 5,
-  exercise_time: 25,
-  exercise_km: 5,
-  exercise_cal: 300,
-  water: 2500,
-  carbs: 400,
-  fat: 80,
-  fiber: 30,
-  protein: 37,
-  sodium: 3,
-  sugar: 50,
-  cholesterol: 20,
-  diet_cal: 2800,
-  alcohol_ml: 60,
-  logdate: '2019-06-01' }
+  heart_rate: rand(65..105),
+  steps: rand(5500..9999),
+  active_minutes: rand(10..30),
+  exercise_type: exercise_array[rand(0..5)],
+  floors: rand(2..4),
+  exercise_time: rand(8..25),
+  exercise_km: rand(1..8),
+  exercise_cal: rand(1900..2800),
+  water: rand(2000..3800),
+  carbs: rand(80..250),
+  fat: rand(80..150),
+  fiber: rand(15..30),
+  protein: rand(25..50),
+  sodium: rand(3..10),
+  sugar: rand(30..120),
+  cholesterol: rand(15..35),
+  diet_cal: rand(2300..3300),
+  alcohol_ml: rand(45..100),
+  logdate: '2019-06-01'}
 
 good_hash = { user: user,
   bmi: 30,
