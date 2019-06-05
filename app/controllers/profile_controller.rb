e2763c655666cef32a8a45a1b34e27cba5867ab9
Thomas Ferris <thomaskeithferris@gmail.com>
2019-06-05 13:31:52 -0400

    init

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,6 +1,6 @@
 class ProfileController < ApplicationController
     def index
     @user = current_user
-    # @health_score = @user.user_scores.last.health_score
+    @health_score = FitbitScore.where("user_id = #{current_user.id}")
     end
 end
