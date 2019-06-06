0803a6ff575847955b803b251cd9e6c816ba6d3b
Thomas Ferris <thomaskeithferris@gmail.com>
2019-06-06 11:10:36 -0400

    quick fix

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,9 +1,9 @@
 class ProfileController < ApplicationController
     def index
-    @user = current_user
-    if @user.fitbit_user_id.nil?
-        redirect_to apis_path
-    end
+     @user = current_user
+    # if @user.fitbit_user_id.nil?
+    #     redirect_to apis_path
+    # end
     @health_score = FitbitScore.where("user_id = #{current_user.id}").last
     end
 end
