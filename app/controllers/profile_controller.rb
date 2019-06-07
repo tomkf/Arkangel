109cbdc00682ecce29920fc8e28c7b4bd3b4933f
Gustaf Folkmar <gustaf@folkmar.se>
2019-06-07 09:01:34 -0400

    Update profile_controller.rb

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -2,9 +2,9 @@ class ProfileController < ApplicationController
     def index
 
      @user = current_user
-    if @user.fitbit_user_id.nil?
-        redirect_to apis_path
-    end
+    # if @user.fitbit_user_id.nil?
+    #     redirect_to apis_path
+    # end
     @user = current_user
     @health_score = FitbitScore.where("user_id = #{current_user.id}").order(:logdate).last
     @parmas_data = UserParam.where("user_id = #{current_user.id}").last
