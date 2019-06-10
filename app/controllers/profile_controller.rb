ebed35c1ee521bfbf60807ad2dae882ff965b4f8
Jose Zea <josedavid.zea@gmail.com>
2019-06-10 14:01:59 -0400

    last commit

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,9 +1,9 @@
 class ProfileController < ApplicationController
     def index
 
-     @user = current_user
+    @user = current_user
     if @user.fitbit_scores.order(:logdate).last.nil?
-       redirect_to apis_path
+      redirect_to apis_path
     end
     @user = current_user
     @health_score = FitbitScore.where("user_id = #{current_user.id}").order(:logdate).last
